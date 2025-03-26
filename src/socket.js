b25828ce948fe18402374c00a8fdece41731ccfb
Mikias-Miessa <mikiasmiessa@gmail.com>
2025-03-27 00:07:51 +0300

    fix: cors error

--- src/socket.js
+++ src/socket.js
@@ -3,4 +3,6 @@ import { io } from "socket.io-client";
 export const socket = io(import.meta.env.VITE_API_URL, {
   autoConnect: true,
   reconnection: true,
+  withCredentials: true,
+  transports: ["websocket", "polling"],
 });
